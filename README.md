7ae7d1f251e0c5c921b0d725c138a3a4ecb88d04
Adrian Statescu <mergesortv@gmail.com>
2014-09-24 11:19:10 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+#  Minimum Spanning Tree
+ 
+   Given a connected, undirected graph, a spanning tree of that graph is subgraph that is tree and connects all the vertices together.
+   A single graph can have many different spanning trees. We can also assign a weight to each edge, which is a number representing
+   how unfavorable it is, and use this to assign a weight to a spanning tree by computing the sum of the weights of the edges in that spanning tree.
+   A Minimum Spanning Tree (MST) or minimum weight spanning tree is then a spanning tree with weight less than or equal to the weight or
+   every other spanning tree. More generally, any undirected graph (not necessarily connected) has a minimum spanning forest, which is union
+    of minimum spanning trees for its connected components.
+
+   ![Screenshot](http://farm3.staticflickr.com/2943/15336251541_1e574aff63.jpg) 
+
+## References
+
+   http://en.wikipedia.org/wiki/Minimum_spanning_tree  
+ 
+ 
+
---
+++ apm.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#define INFINIT 888
+#define FIN "apm.in"
+#define FOUT "apm.out"
+#define MAX 15000
+
+int num_nodes, num_edges;
+int S[ MAX ], F[ MAX ];
+long int cost = 0;
+int mat[ MAX ][ MAX ];
+
+void read() 
+{
+     int i,
+         j,
+         c;
+
+     freopen(FIN, "r", stdin);      
+
+     scanf("%d %d", &num_nodes, &num_edges); 
+
+     for(i = 1; i <= num_nodes; i++) {
+
+         for(j = 1; j <= num_nodes; j++) {
+
+             if( i == j) { 
+
+                   mat[i][j] = 0;
+
+             } else {
+
+                   mat[i][j] = INFINIT;
+             }
+        }
+     }
+
+      
+     while(num_edges--) {
+
+           scanf("%d %d %d", &i, &j, &c);
+
+           mat[ i ][ j ] = c;     
+           mat[ j ][ i ] = c;
+     } 
+
+     fclose( stdin ); 
+
+};
+
+void matrix() {
+
+     int i,j;
+
+     for(i = 1; i <= num_nodes; i++) {
+
+         for(j = 1; j <= num_nodes; j++) {
+
+             printf("%5d ",mat[i][j]);
+         }
+
+         printf("\n");
+     }
+};
+
+void solve() {
+
+     int i,
+         j,
+         k, min, posMin;  
+
+     int start;
+
+     start = 1;
+ 
+     for(i = 1; i <= num_nodes; i++) {
+
+         if(i == start) S[ i ] = 0;
+
+                 else   
+
+                        S[ i ] = start;
+     }   
+
+     for(i = 1; i <= num_nodes - 1; i++) {
+
+             min = INFINIT;      
+         
+             for(j = 1; j <= num_nodes; j++) {
+
+                 if(S[ j ]) {
+
+                             if( mat[ S[j] ][ j ] < min) {
+
+                                 min = mat[ S[j] ][ j ];
+
+                                 posMin = j; 
+                             }               
+                 }
+                  
+             } 
+
+
+             F[ posMin ] = S[ posMin ]; 
+
+             cost += mat[S[posMin]][posMin]; 
+
+             S[ posMin ] = 0;
+ 
+             //updated the rest of the nodes
+             for(j = 1; j <= num_nodes; j++){
+
+                     if(S[j] && mat[ posMin ][ j ] < mat[ S[j] ][ j ] ) S[j] = posMin;  
+             }
+     }     
+
+     freopen(FOUT, "w", stdout);      
+
+     printf("%d\n%d\n", cost, num_nodes-1);
+
+             for(j = 1; j <= num_nodes; j++){
+
+                      if(F[j]) printf("%d %d\n", j, F[j]);
+
+             }
+     fclose( stdout ); 
+
+};
+
+int main() 
+{
+ 
+    read();  
+    solve();
+
+    return(0);
+};
